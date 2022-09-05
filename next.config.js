8359e4ee7f6dd0cfcbe5c2766a8d4dfc98d40855
Andrew <growler625@gmail.com>
2022-09-05 17:40:53 +0300

    add cockpit to project

--- next.config.js
+++ next.config.js
@@ -9,9 +9,16 @@
 const withReactSvg = require('next-react-svg')
 const path = require('path')
 
+const env = {
+  cockpitApiUrl: process.env.API_URL,
+  cockpitApiToken: process.env.API_TOKEN,
+  cockpitBaseUrl: process.env.BASE_URL
+}
+
 module.exports = withReactSvg({
+  env,
   include: path.resolve(__dirname, 'src/assets/svg'),
   webpack(config, options) {
     return config
-  }
+  },
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,25 @@ import type { NextPage } from "next";
 import Categories from "../src/components/HomePage/Categories/Categories";
 import Popular from "../src/components/HomePage/Popular/Popular";
 import RegisterBusiness from "../src/components/HomePage/RegisterBusiness/RegisterBusiness";
+import axios from 'axios'
 
 const IndexPage: NextPage = () => {
+  
+
+  // const getData = async () => {
+  //   console.log("process.env")
+  //   console.log(process.env.cockpitApiUrl)
+
+  //   const { data } = await axios.get(`${process.env.cockpitApiUrl}/collections/get/Businesses?token=${process.env.cockpitApiToken}`)
+
+  //   console.log("data");
+  //   console.log(data);
+    
+  //   return data
+  // }
+
+  // getData();
+
   return (
     <>
       <Categories />
