305cab09b0ff138a6937fdbfd2d0a4ea5a524339
VadymDenysiuk <80970410371q@gmail.com>
2022-09-07 12:37:28 +0300

    change business card

--- src/components/HomePage/Popular/Popular.tsx
+++ src/components/HomePage/Popular/Popular.tsx
@@ -5,26 +5,28 @@ import IconButton from "../../shared/IconButton";
 import PopularCards from "../../../constants/popular";
 import PopularCard from "../../shared/BusinessCard";
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 const Popular = () => {
   const [cards, setCards] = useState<any>([]);
 
   const getBusinesses = async () => {
-    const { data } = await axios.get(`${process.env.cockpitApiUrl}/collections/get/Businesses?token=${process.env.cockpitApiToken}&limit=4`)
+    const { data } = await axios.get(
+      `${process.env.cockpitApiUrl}/collections/get/Businesses?token=${process.env.cockpitApiToken}&limit=4`,
+    );
 
     if (data) {
-      setCards(data.entries)
-      return data.entries
+      setCards(data.entries);
+      return data.entries;
     }
 
-    setCards([])
+    setCards([]);
     return [];
-  }
-  
+  };
+
   useEffect(() => {
-    getBusinesses()
-  }, [])
+    getBusinesses();
+  }, []);
 
   return (
     <section className="popular">
@@ -46,14 +48,30 @@ const Popular = () => {
 
         <h2 className="popular__title title">Найпопулярніші</h2>
         <ul className="popular__cards">
-          {cards.map(({ _id, title, description, images }: any) => (
-            <PopularCard
-              key={_id}
-              title={title}
-              description={description}
-              image={`http://157.230.99.45:8082${images[0].path}`}
-            />
-          ))}
+          {cards /* PopularCards */
+            .map(
+              ({
+                _id,
+                title,
+                description,
+                images /* ,
+                price,
+                views,
+                isVerified, */,
+              }: any) => (
+                <PopularCard
+                  key={_id}
+                  title={title}
+                  description={description}
+                  image={
+                    /* images */ `http://157.230.99.45:8082${images[0].path}`
+                  }
+                  // price={price}
+                  // views={views}
+                  // isVerified={isVerified}
+                />
+              ),
+            )}
         </ul>
       </div>
     </section>
