75d64418a852c40064b56d932cfec92d5de3df17
VadymDenysiuk <80970410371q@gmail.com>
2022-12-01 11:35:35 +0200

    add number formation to price

--- src/utils/useUah.tsx
+++ src/utils/useUah.tsx
@@ -1,7 +1,9 @@
 const UseUah = (currency: string, price: string, rate: number) => {
-  return currency == "Гривня"
-    ? `${Number(price).toFixed(0)} грн`
-    : `${(Number(price) * rate).toFixed(0)} грн`;
+  const uah = Number(price).toFixed(0);
+  const usd = (Number(price) * rate).toFixed(0);
+  const uahFmt = new Intl.NumberFormat("ua-UA").format(Number(uah));
+  const usdFmt = new Intl.NumberFormat("ua-UA").format(Number(usd));
+  return currency == "Гривня" ? `${uahFmt} грн` : `${usdFmt} грн`;
 };
 
 export default UseUah;
--- src/utils/useUsd.tsx
+++ src/utils/useUsd.tsx
@@ -1,7 +1,10 @@
 const UseUsd = (currency: string, price: string, rate: number) => {
-  return currency == "Гривня"
-    ? `${(Number(price) / rate).toFixed(0)} $`
-    : `${Number(price).toFixed(0)} $`;
+  const uah = (Number(price) / rate).toFixed(0);
+  const usd = Number(price).toFixed(0);
+  const uahFmt = new Intl.NumberFormat("ua-UA").format(Number(uah));
+  const usdFmt = new Intl.NumberFormat("ua-UA").format(Number(usd));
+
+  return currency == "Гривня" ? `${uahFmt} $` : `${usdFmt} $`;
 };
 
 export default UseUsd;
