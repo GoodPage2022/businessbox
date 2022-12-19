9d1981c37bd42d1e9cde81c38ee80fea63ea79a3
VadymDenysiuk <80970410371q@gmail.com>
2022-12-19 13:42:25 +0200

    add privacy-policy

---
+++ pages/information/privacy-policy.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import React from "react";
+import PrivacyPolicy from "../../src/components/Information/PrivacyPolicy";
+
+const PrivacyPolicyPage: NextPage = () => {
+  return <PrivacyPolicy />;
+};
+
+export default PrivacyPolicyPage;
