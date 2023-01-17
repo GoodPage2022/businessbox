cca990da72572026de7feeb46c226807fafbc4a9
VadymDenysiuk <80970410371q@gmail.com>
2023-01-17 16:10:16 +0200

    fix type

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,43 +1,43 @@
 import GoogleProvider from "next-auth/providers/google";
 import type { Profile, TokenSet, User, Awaitable } from "next-auth";
-import NextAuth from "next-auth/next"
+import NextAuth from "next-auth/next";
 
 export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.clientId ?? "",
-            clientSecret: process.env.clientSecret ?? "",
-            profile(profile) {
-                return {
-                  id: profile.sub,
-                  name: profile.name,
-                  email: profile.email,
-                  image: profile.picture,
-                  given_name: profile.given_name,
-                  family_name: profile.family_name,
-                }
-            },
-        }),
-    ],
-    secret: process.env.jwtSecret,
-    callbacks: {
-        async session({ session, token, user }) {            
-            session.given_name = token.given_name
-            session.family_name = token.family_name
-            session.accessToken = token.accessToken;
+  providers: [
+    GoogleProvider({
+      clientId: process.env.clientId ?? "",
+      clientSecret: process.env.clientSecret ?? "",
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          given_name: profile.given_name,
+          family_name: profile.family_name,
+        };
+      },
+    }),
+  ],
+  secret: process.env.jwtSecret,
+  callbacks: {
+    async session({ session, token, user }: any) {
+      session.given_name = token.given_name;
+      session.family_name = token.family_name;
+      session.accessToken = token.accessToken;
 
-            return session;
-        },
-        async jwt({ token, user, account, profile, isNewUser }) {
-            if (user) {
-                token.id = user.id;
-                token.given_name = user.given_name
-                token.family_name = user.family_name
-            }
-            if (account) {
-                token.accessToken = account.access_token;
-            }
-            return token;
-        },
-    }
-})
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }: any) {
+      if (user) {
+        token.id = user.id;
+        token.given_name = user.given_name;
+        token.family_name = user.family_name;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
+});
