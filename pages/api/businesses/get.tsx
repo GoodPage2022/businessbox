a6e260e231ad2c3bdb7035ae401e420ff94e722e
VadymDenysiuk <80970410371q@gmail.com>
2023-02-24 16:23:25 +0200

    add console log to get business

--- pages/api/businesses/get.tsx
+++ pages/api/businesses/get.tsx
@@ -1,23 +1,32 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-const handler = async(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) => {
-  const token = (req.body.user != null && req.body.user?.api_key !== undefined) ? req.body.user.api_key : process.env.cockpitApiToken
-  const id = req.body.projectId
+const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  const token =
+    req.body.user != null && req.body.user?.api_key !== undefined
+      ? req.body.user.api_key
+      : process.env.cockpitApiToken;
+  const id = req.body.projectId;
+
+  console.log(token, "dsfsdfsdf");
+  console.log(id, "id, ");
 
   try {
-    const response = await axios.get(`${process.env.cockpitApiUrl}/collections/get/Businesses?token=${token}&filter[_id]=${id}`)
-    res.status(200).json( response.data )
-  } catch (error: any) {
-    res.status(500).json({error})
-  }
-}
+    const response = await axios.get(
+      `${process.env.cockpitApiUrl}/collections/get/Businesses?token=${token}&filter[_id]=${id}`,
+    );
+    console.log(response, "response");
+    console.log(response.data, "response.data");
 
-export default handler
+    res.status(200).json(response.data);
+  } catch (error: any) {
+    console.log(error, "error");
+    res.status(500).json({ error });
+  }
+};
+
+export default handler;
