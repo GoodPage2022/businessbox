6e9c4acc5135b01e6e7d40c1208401f2fa2ebadf
Andrew <growler625@gmail.com>
2023-07-07 18:54:55 +0300

    add core js at

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import { useRouter } from "next/router";
 
 import * as fbq from "../lib/fpixel";
 import { useEffect } from "react";
+import 'core-js/features/array/at'
 
 function MyApp({ Component, pageProps, session }: any) {
   // let persistor = persistStore(store);
