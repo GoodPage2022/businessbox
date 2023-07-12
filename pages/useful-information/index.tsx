0e73070d58cba063bf4023f54d7501e8c5ba5bcb
VadymDenysiuk <80970410371q@gmail.com>
2023-07-12 10:02:54 +0300

    fix og description for useful-information

--- pages/useful-information/index.tsx
+++ pages/useful-information/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 // import { useRouter } from "next/router";
 
 import UsefulTools from "../../src/components/UsefulTools/UsefulTools";
+import Head from "next/head";
 
 const UsefulToolsPage: NextPage = () => {
   // const router = useRouter();
@@ -14,7 +15,18 @@ const UsefulToolsPage: NextPage = () => {
   //   return <></>;
   // }
 
-  return <UsefulTools />;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Чеклист" />
+        <meta
+          property="og:description"
+          content="Як підготувати бізнес до продажу або залучення інвестицій"
+        />
+      </Head>
+      <UsefulTools />;{" "}
+    </>
+  );
 };
 
 export default UsefulToolsPage;
