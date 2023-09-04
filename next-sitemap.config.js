6e5241bde92a54d9312ea3c682605a6bcc86ffd6
Andrew <growler625@gmail.com>
2023-09-04 13:45:51 +0300

    sitemap transform

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -27,6 +27,26 @@ module.exports = {
 
     //   return result
     // },
+    transform: async (config, path) => {
+      // only create changefreq along with path
+      // returning partial properties will result in generation of XML field with only returned values.
+      if (customLimitedField(path)) {
+        // This returns `path` & `changefreq`. Hence it will result in the generation of XML field with `path` and  `changefreq` properties only.
+        return {
+          loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+          changefreq: 'weekly',
+        }
+      }
+  
+      // Use default transformation for all other cases
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      }
+    },
     robotsTxtOptions: {
       additionalSitemaps: [
         'https://bissbox.com/sitemap-catalog.xml',
