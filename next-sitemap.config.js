5a257f402aa65dbc981b41fb971e103c68cd3dc9
Andrew <growler625@gmail.com>
2023-09-04 13:59:28 +0300

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -11,26 +11,7 @@ module.exports = {
       '/account/inactive-businesses',
       '/account/my-businesses',
     ],
-    transform: async (config, path) => {
-      // only create changefreq along with path
-      // returning partial properties will result in generation of XML field with only returned values.
-      if (customLimitedField(path)) {
-        // This returns `path` & `changefreq`. Hence it will result in the generation of XML field with `path` and  `changefreq` properties only.
-        return {
-          loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-          changefreq: 'weekly',
-        }
-      }
-  
-      // Use default transformation for all other cases
-      return {
-        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-        changefreq: config.changefreq,
-        priority: config.priority,
-        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-        alternateRefs: config.alternateRefs ?? [],
-      }
-    },
+    changefreq: 'weekly',
     robotsTxtOptions: {
       additionalSitemaps: [
         'https://bissbox.com/sitemap-catalog.xml',
