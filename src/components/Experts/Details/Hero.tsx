64b4a2485a577156784dde595784ad05924b4bde
VadymDenysiuk <80970410371q@gmail.com>
2023-09-05 10:43:09 +0300

    add breadcrumbs to the expert

--- src/components/Experts/Details/Hero.tsx
+++ src/components/Experts/Details/Hero.tsx
@@ -1,8 +1,39 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const HeroExpert = ({ data }: any) => {
   return (
     <div className="heroExpert">
+      <ul className="breadcrumbs container">
+        <li className="breadcrumbs__item section__secondary-text">
+          <Link href="/">
+            <a>
+              Головна
+              <span className="breadcrumbs__arrow">{">"}</span>
+            </a>
+          </Link>
+        </li>
+        <li className="breadcrumbs__item section__secondary-text">
+          <Link href="/experts">
+            <a>
+              Каталог
+              <span className="breadcrumbs__arrow"> {">"}</span>
+            </a>
+          </Link>
+        </li>
+        <li className="breadcrumbs__item section__secondary-text">
+          <Link href={`/experts/specialization/${data.field_of_expertise[0]}`}>
+            <a>
+              {data.field_of_expertise[0]}
+              <span className="breadcrumbs__arrow">{">"}</span>
+            </a>
+          </Link>
+        </li>
+        <li className="breadcrumbs__item section__secondary-text">
+          {" "}
+          {data.firstname} {data.lastname}
+        </li>
+      </ul>
       <div className="heroExpert__container container">
         <div className="heroExpert__image">
           <Image
