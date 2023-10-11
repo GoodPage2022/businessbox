94a3abc302cc6eff767b394c35202e9de0a8c1a9
VadymDenysiuk <80970410371q@gmail.com>
2023-10-11 17:26:44 +0300

    add sold page

--- pages/catalog/[project].tsx
+++ pages/catalog/[project].tsx
@@ -1,21 +1,34 @@
 import type { NextPage } from "next";
 import React from "react";
 import ProjectInfo from "../../src/components/Catalog/ProjectInfo/ProjectInfo";
-import { useRouter } from "next/router";
 import Head from "next/head";
 import axios from "axios";
 
-const Project: NextPage = ({project}:any) => {
+const Project: NextPage = ({ project, isSold }: any) => {
   if (!project || typeof project?._id != "string") return <></>;
 
   return (
     <>
       <Head>
-          <title>{`Business Box | ${project.title}`}</title>
-          <meta property="og:title" content={`Business Box | ${project.title}`} />
-          <meta property="og:description" content={project.description.replace(/(<([^>]+)>)/gi, "").substring(0, 250)} />
+        <title>{`Business Box | ${project.title}`}</title>
+        <meta property="og:title" content={`Business Box | ${project.title}`} />
+        <meta
+          property="og:description"
+          content={project.description
+            .replace(/(<([^>]+)>)/gi, "")
+            .substring(0, 250)}
+        />
       </Head>
-      <ProjectInfo projectId={project._id} />
+      {isSold ? (
+        <section className="projectInfo">
+          <div className="container projectInfo__container">
+            {" "}
+            <div className="projectInfo__sold title">Бізнес було продано</div>
+          </div>{" "}
+        </section>
+      ) : (
+        <ProjectInfo projectId={project._id} />
+      )}
     </>
   );
 };
@@ -23,28 +36,34 @@ const Project: NextPage = ({project}:any) => {
 export default Project;
 
 export async function getServerSideProps(context: any) {
-  const { project } = context.params
+  const { project } = context.params;
 
   try {
-    const response = await axios.post(`${process.env.baseUrl}/api/businesses/get`, { user: null, projectId: project });
+    const response = await axios.post(
+      `${process.env.baseUrl}/api/businesses/get`,
+      { user: null, projectId: project }
+    );
     if (response.data) {
+      if (response.data.entries[0].sold_out) {
+        return { props: { isSold: true, project: response.data.entries[0] } };
+      }
       if (response.data.entries.length > 0) {
         return {
-            props: {
-              project: response.data.entries[0]
-            },
-        }
+          props: {
+            project: response.data.entries[0],
+          },
+        };
       }
     }
-  
+
     return {
-      notFound: true
-    }
+      notFound: true,
+    };
   } catch (error: any) {
     console.log(error.response.status);
-    
+
     return {
-      notFound: true
-    }
+      notFound: true,
+    };
   }
 }
